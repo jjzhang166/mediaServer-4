02d18a2fef7661cb950ed89ba396a3f5b0fb8809
qinXpeng <qinpengx@gmail.com>
2020-08-17 00:48:19 +0800

    commit first day

---
+++ server/server.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<unistd.h>
+#include "server.h"
+namespace qp_Server{
+
+Server::Server(){
+	std::cout<<"construct.."<<std::endl;
+}
+Server::~Server(){
+	std::cout<<"destruct.."<<std::endl;
+}
+void Server::run(){
+	while(true){
+		std::cout<<"running.."<<std::endl;
+		::usleep(1000000);//sleep 1s;
+	}
+}
+
+
+};
---
+++ server/server.h
@@ -0,0 +1,17 @@
+/**
+ * Server class 
+ * @author qin_peng
+ * @date   2020-8-16
+ */
+#ifndef __SERVER_H__
+#define __SERVER_H__
+namespace qp_Server{
+class Server{
+public:
+	Server();
+	~Server();
+public:	
+	void run();
+}; 
+};
+#endif //__SERVER_H__
